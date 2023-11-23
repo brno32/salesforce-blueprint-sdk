fa48e9fe6108fdb2d5bf575d4a9b5574bbc835ba
Alex Drozd <drozdster@gmail.com>
2023-11-23 22:38:25 +0100

    fixing xml for soap login

--- Source/SalesforceBlueprintSDK/Private/SalesforceLogin.cpp
+++ Source/SalesforceBlueprintSDK/Private/SalesforceLogin.cpp
@@ -25,26 +25,26 @@ void USalesforceLogin::Activate()
     args.Add(FStringFormatArg(SecurityToken));
 
     FString LoginSoapRequestBody = FString::Format(
-        TEXT(
-            "<?xml version='1.0' encoding='utf-8' ?><env:Envelope"
-            "    xmlns:xsd='http://www.w3.org/2001/XMLSchema'"
-            "    xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'"
-            "    xmlns:env='http://schemas.xmlsoap.org/soap/envelope/'"
-            "    xmlns:urn='urn:partner.soap.sforce.com'>"
-            "<env:Header>"
-            "    <urn:CallOptions>"
-            "        <urn:client>{0}</urn:client>"
-            "        <urn:defaultNamespace>sf</urn:defaultNamespace>"
-            "    </urn:CallOptions>"
-            "</env:Header>"
-            "<env:Body>"
-            "    <n1:login xmlns:n1='urn:partner.soap.sforce.com'>"
-            "        <n1:username>{1}</n1:username>"
-            "        <n1:password>{2}{3}</n1:password>"
-            "    </n1:login>"
-            "</env:Body>"
-            "</env:Envelope>"
-        ),
+		TEXT(R"(<?xml version="1.0" encoding="utf-8" ?>
+<env:Envelope
+    xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xmlns:env="http://schemas.xmlsoap.org/soap/envelope/"
+    xmlns:urn="urn:partner.soap.sforce.com">
+    <env:Header>
+        <urn:CallOptions>
+            <urn:client>{0}</urn:client>
+            <urn:defaultNamespace>sf</urn:defaultNamespace>
+        </urn:CallOptions>
+    </env:Header>
+    <env:Body>
+        <n1:login xmlns:n1="urn:partner.soap.sforce.com">
+            <n1:username>{1}</n1:username>
+            <n1:password>{2}{3}</n1:password>
+        </n1:login>
+    </env:Body>
+</env:Envelope>
+)"),
         args
     );
 
